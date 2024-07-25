79f4114de0421a74d74f1a480cf2b527e250b66b
ofuron72 <pereligin.3004@gmail.com>
2024-07-25 17:58:35 +0300

    hfh

--- src/Regex/Regex1.java
+++ src/Regex/Regex1.java
@@ -1,5 +1,8 @@
 package Regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex1 {
     public static void main(String[] args) {
         String s = "Ivanov Vasiliy, Russia, Moscow, Lenin street, 51, Flat 48,"+
@@ -8,5 +11,15 @@ public class Regex1 {
                 " email: masha@yandex.ru, Postcode: UKR54, Phone Number: +987654321; "+
                 "Chuck Norris, USA, Hollywood, All stars street, 87, Flat 21,"+
                 " email: chuck@gmail.ru, Postcode: USA23, Phone Number: +136478952; ";
+
+      //  Pattern pattern = Pattern.compile("\\w+");
+       // Pattern pattern  = Pattern.compile("\\b\\d{2}\\b");
+        // Pattern pattern  = Pattern.compile("\\+\\d{9}"); //number
+         Pattern pattern  = Pattern.compile("\\w+@\\w+\\.\\w+"); //email
+        Matcher matcher = pattern.matcher(s);
+
+        while(matcher.find()){
+            System.out.println(matcher.group());
+        }
     }
 }
---
+++ src/Regex/Regex3.java
@@ -0,0 +1,7 @@
+package Regex;
+
+public class Regex3 {
+    public static void main(String[] args) {
+        String s2
+    }
+}
