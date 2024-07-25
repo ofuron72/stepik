a7280ecadf5a6ac3cc4d52ff7a481a3eba34e7f2
ofuron72 <pereligin.3004@gmail.com>
2024-07-25 13:13:10 +0300

    regex add

---
+++ src/workWithFiles/myFileVisitor2.java
@@ -0,0 +1,29 @@
+package workWithFiles;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class myFileVisitor2 extends SimpleFileVisitor<Path> {
+    Path source;
+    Path destination;
+
+    public myFileVisitor2(Path source, Path destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+      Path newDestination = destination.resolve(source.relativize(dir));
+        Files.copy(dir, newDestination, StandardCopyOption.REPLACE_EXISTING);
+        return FileVisitResult.CONTINUE;
+
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(file));
+        Files.copy(file, newDestination, StandardCopyOption.REPLACE_EXISTING);
+        return FileVisitResult.CONTINUE;
+    }
+}
